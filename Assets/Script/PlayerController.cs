3ad74fb0e1373ed6efd8cce707d924b0a2978296
QuinnSuligoy <56174399+QuinnSuligoy@users.noreply.github.com>
2019-11-25 13:23:43 -0800

    Added Movement and Boundries

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horzInput;
+    private float speed = 10f;
+    private float Xrange = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get Input
+        horzInput = Input.GetAxis("Horizontal");
+        //Move Player
+        transform.Translate(Vector3.right * horzInput * Time.deltaTime * speed);
+        //Boundries
+        if (transform.position.x < -Xrange)
+        {
+            transform.position = new Vector3(-Xrange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > Xrange)
+        {
+            transform.position = new Vector3(Xrange, transform.position.y, transform.position.z);
+        }
+    }
+}
